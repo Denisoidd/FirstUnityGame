b64a2b9f272dd2a26b6838ba4fbf2be0869a0557
Denisoidd <denis.chaynikov@phystech.edu>
2021-07-17 08:54:27 +0200

    Added weapon rotation by mouse y axe

--- Assets/Scripts/RotateWeapon.cs
+++ Assets/Scripts/RotateWeapon.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateWeapon : MonoBehaviour
-{   
+{
+    public Transform gun;
+    public float rotationSpeed = 200;
+
+    private float moveY;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        moveY = Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+        gun.Rotate(0, 0, moveY, Space.Self);
     }
 }
