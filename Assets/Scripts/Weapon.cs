ea8ed8415c5ae4c6657ac6efbce597881b031f56
Denisoidd <denis.chaynikov@phystech.edu>
2021-07-17 08:15:38 +0200

    Added shooting mechanics

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public Rigidbody bullet;
+    public Transform gunFireTransform;
+    public float bulletSpeed = 10.0f;
+
+    private bool isShooting;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,24 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isShooting = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (isShooting)
+        {
+            Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, gunFireTransform.position, bullet.transform.rotation);
+            bulletClone.AddForce(gunFireTransform.right * bulletSpeed, ForceMode.Impulse);
+        }
+        isShooting = false;
     }
 }
