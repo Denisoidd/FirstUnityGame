38d6b342ba74353a726c771fa4257030a5b6822d
Denisoidd <denis.chaynikov@phystech.edu>
2021-07-20 13:57:43 +0200

    Added ricoshet bullet effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
+    public Rigidbody rigidBodyComponent;
+    
+    [Range(0, 100)]
+    public float ricochetForce = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision col)
     {
-        
+        rigidBodyComponent.AddForce(col.contacts[0].normal * ricochetForce, ForceMode.Impulse);
     }
 }
