10ab5c48580cc0ef436f8641d5c88936fb64a244
Denisoidd <denis.chaynikov@phystech.edu>
2021-07-20 14:06:02 +0200

    Added ricoshet functionality with destruction limit

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,15 +9,33 @@ public class Bullet : MonoBehaviour
     
     [Range(0, 100)]
     public float ricochetForce = 50f;
+    
+    private int maxRicochet = 2;
+    private int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBodyComponent = GetComponent<Rigidbody>();
+        Invoke("DisableBullet", 7f);
     }
 
     void OnCollisionEnter(Collision col)
     {
-        rigidBodyComponent.AddForce(col.contacts[0].normal * ricochetForce, ForceMode.Impulse);
+        if (counter < maxRicochet)
+        {
+            rigidBodyComponent.AddForce(col.contacts[0].normal * ricochetForce, ForceMode.Impulse);
+            counter++;
+        }
+        else
+        {
+            DisableBullet();
+        }
+        
+    }
+    
+    void DisableBullet()
+    {
+        Destroy(gameObject);
     }
 }
